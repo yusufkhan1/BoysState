5d1d6aa5399f4318115743bbd6b0b6a905ba5be9
Dash Wedergren <Dash-Wedergren@users.noreply.github.com>
2018-06-05 10:23:34 -0500

    Template Python

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-import input
 from json import load, loads
+import mysql.connector
 
-query_params = input("Enter comma dilentiated list of params:")
+config = {
+  'user': 'scott',
+  'password': 'password',
+  'host': '127.0.0.1',
+  'database': 'employees',
+  'raise_on_warnings': True,
+}
 
-print(loads(query_params))
+cnx = mysql.connector.connect(**config)
+
+cursor = cnx.cursor()
+
+query = ("SELECT first_name, last_name, hire_date FROM employees "
+         "WHERE hire_date BETWEEN %s AND %s")
+
+hire_start = datetime.date(1999, 1, 1)
+hire_end = datetime.date(1999, 12, 31)
+
+cursor.execute(query, (hire_start, hire_end))
+
+cursor.clos()
+cnx.close()
+
+# For rquest to /query expect a json object that references a dict key: sql field, value: typed request
+# Return Ordered list of full entires
+# Display Stater id, first, last, hometown, Boys' State Year
